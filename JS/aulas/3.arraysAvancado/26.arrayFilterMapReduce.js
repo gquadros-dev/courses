5dcd59ecffbe2cdfb7b2b85b3e5eb81f7ada393e
Gabriel Quadros <gabrielfernand464508@gmail.com>
2023-10-06 18:00:15 -0300

    aprendendo filter

---
+++ JS/aulas/3.arraysAvancado/26.arrayFilterMapReduce.js
@@ -0,0 +1,27 @@
+// Filter, map, reduce
+
+// Retorne os números maiores que 10
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosFiltrados = numeros.filter(valor => valor > 10);
+
+
+console.log(numerosFiltrados);
+
+
+
+
+
+
+
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Letícia', idade: 19 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+]
