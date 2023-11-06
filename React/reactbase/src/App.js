1095c992876fc39fe18c0538c20fd8bf0f7855e8
Gabriel Quadros <gabrielfernand464508@gmail.com>
2023-11-06 15:53:59 -0300

    começando react router, toastify e Redux

--- React/reactbase/src/App.js
+++ React/reactbase/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import history from './services/history';
+import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </Router>
+    </Provider>
   );
 }
 
---
+++ React/reactbase/src/store/modules/example/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  botaoClicado: false,
+};
+
+export default function (state = initialState, action) {
+  switch(action.type){
+    case 'BOTAO_CLICADO':
+      const newState = { ...state };
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+
+    default:
+      return state;
+  }
+};
