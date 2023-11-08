1e4a53e035e0b8fcd907e5e97ddbf48216eb1b81
Gabriel Quadros <gabrielfernand464508@gmail.com>
2023-11-08 16:58:33 -0300

    resolvido problema puxando images

--- React/consumoAPI/src/pages/Alunos/index.js
+++ React/consumoAPI/src/pages/Alunos/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import { get } from 'lodash';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
-import { AlunoContainer } from './styled';
+import { AlunoContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
 
 export default function Alunos() {
@@ -23,7 +26,18 @@ export default function Alunos() {
       <AlunoContainer>
         {alunos.map(aluno => (
           <div key={String(aluno.id)}>
-            <img src={aluno.Images[0].url} alt="" />
+            <ProfilePicture>
+              {get(aluno, 'Images[0].url', false) ? (
+              <img crossOrigin="" src={aluno.Images[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+
+            <Link to={`/aluno/${aluno.id}/edit`}><FaEdit size={16} /></Link>
+            <Link to={`/aluno/${aluno.id}/delete`}><FaWindowClose size={16} /></Link>
           </div>
         ))}
       </AlunoContainer>
--- React/consumoAPI/src/pages/Alunos/styled.js
+++ React/consumoAPI/src/pages/Alunos/styled.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export const AlunoContainer = styled.div`
-  
+  margin-top: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+
+  div + div {
+    border-top: 1px solid #DDD;
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
 `;
