ff81d54c585b66344adfcd730f3f0470084e209e
Gabriel Quadros <gabrielfernand464508@gmail.com>
2023-11-09 17:59:27 -0300

    modificaçõezinhas

--- React/consumoAPI/src/pages/Login/index.js
+++ React/consumoAPI/src/pages/Login/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Login() {
   return (
     <Container>
       <h1>Login </h1>
+
+      <Form>
+        
+      </Form>
     </Container>
   );
 }
